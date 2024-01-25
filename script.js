d14e5a0dd023f17c3e30bb718ddfdc2ce04f4a1c
vildeil <vildeludvigsen@hotmail.com>
2024-01-25 13:49:27 +0100

    ferdig
    lagt til kommentarer i script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 let resourcesHTML = ''
+//lager en variabel med en tom streng som blir brukt til å bygge opp HTML-innholdet
 
 resources.map(info => resourcesHTML += `<article>
 <h2>${info.category}</h2>
@@ -9,8 +10,7 @@ resources.map(info => resourcesHTML += `<article>
 </ul>
 </article>`
 )
-
-
+//bruker map for å gå igjennom objektene i arrayen resources, for så å sette verdiene inn i HTML-koden
 
 function showInfo(e) {
     resourcesHTML = ""
@@ -22,6 +22,9 @@ function showInfo(e) {
     })
     e.target.classList.add("active")
     const main = document.getElementsByTagName("main")
+    //lager funksjonen showInfo hvor elementene som har id" menuItem og lager en array ved hjelp av Array.from
+    //kjører en map på menuItems hvor elementer med class="active" blir fjernet.
+    //e.target.classList.add("active") legger til active på elementet som blir klikket på.
     
     resources.map(info => {
         if (e.target.id === info.category) {
@@ -33,9 +36,13 @@ function showInfo(e) {
             </ul>
             </article>`
         }
+    //bruker map på resources arrayen for å generere HTML-kode basert på verdiene fra resources. 
+    //koden sjekker også om kategorien til en ressurs er lik ID-en til elementet som blir klikket på.
     main[0].innerHTML = resourcesHTML
+    //setter HTML-innholdet til main-elementet til resourcesHTML som nå inneholder generert HTML-kode for den
+    //valgte kategorien.
 })
 }
 
 document.getElementById("HTML").click()
-
+//starter visningen med HTML-kategorien som standard.
